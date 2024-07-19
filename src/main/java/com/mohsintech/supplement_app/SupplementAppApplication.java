7620d7109c3941b80e2f977442c6e9cbad6c5d1d
mohsinfarhat96 <mohsinfarhat96@gmail.com>
2024-07-19 17:39:06 -0500

    first commit

---
+++ src/main/java/com/mohsintech/supplement_app/SupplementAppApplication.java
@@ -0,0 +1,13 @@
+package com.mohsintech.supplement_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplementAppApplication.class, args);
+	}
+
+}
