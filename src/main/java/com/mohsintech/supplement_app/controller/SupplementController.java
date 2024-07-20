9ed4b8321fd62184affaccd130098e5fa1024802
mohsinfarhat96 <mohsinfarhat96@gmail.com>
2024-07-20 10:40:51 -0500

    Connect controller to the services.

--- src/main/java/com/mohsintech/supplement_app/controller/SupplementController.java
+++ src/main/java/com/mohsintech/supplement_app/controller/SupplementController.java
@@ -1,14 +1,12 @@
 package com.mohsintech.supplement_app.controller;
 
 import com.mohsintech.supplement_app.dto.SupplementDto;
+import com.mohsintech.supplement_app.dto.SupplementResponse;
 import com.mohsintech.supplement_app.service.SupplementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/")
@@ -27,5 +25,31 @@ public class SupplementController {
         SupplementDto response = supplementService.getSupplement(supplementId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+    @GetMapping("supplement")
+    public ResponseEntity<SupplementResponse> getSupplements(
+            @RequestParam(value = "pageNo", defaultValue = "0", required = false)int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "5", required = false)int pageSize){
+        SupplementResponse response = supplementService.getSupplements(pageNo,pageSize);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("supplement/create")
+    public ResponseEntity<SupplementDto> createPokemon(@RequestBody SupplementDto supplementDto){
+        SupplementDto response = supplementService.createSupplement(supplementDto);
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
+    }
+
+    @PutMapping("supplement/{id}/update")
+    public ResponseEntity<SupplementDto> updateSupplement(@PathVariable(value = "id")int supplementId, @RequestBody SupplementDto supplementDto){
+        SupplementDto response = supplementService.updateSupplement(supplementId,supplementDto);
+        return new ResponseEntity<>(response,HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("supplement/{id}/delete")
+        public ResponseEntity<String> deleteSupplement(@PathVariable(value = "id")int supplementId){
+            supplementService.deleteSupplement(supplementId);
+            return new ResponseEntity<>("Deleted Successfully",HttpStatus.OK);
+        }
 
 }
