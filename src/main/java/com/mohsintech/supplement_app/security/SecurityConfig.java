bfe69084f009886ef09a902fb8e5050ab9d2507e
mohsinfarhat96 <mohsinfarhat96@gmail.com>
2024-07-22 12:34:07 -0500

    feat: started security implementation

---
+++ src/main/java/com/mohsintech/supplement_app/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.mohsintech.supplement_app.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //this annotation tells Spring that we'll be creating our own security instead of defaults
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+}
